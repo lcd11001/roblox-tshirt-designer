61dbd4773e438c30b7ec1e20ef1091d08460b208
Luong Cong Dan <dan.luongcong@gameloft.com>
2026-03-18 15:30:57 +0700

    * use Profile Service for profile events on both server + client

--- src/ServerScriptService/Profile/ProfileTemplate.lua
+++ src/ServerScriptService/Profile/ProfileTemplate.lua
@@ -1,5 +1,7 @@
 -- The PROFILE_TEMPLATE table is what new profile "Profile.Data" will default to:
 local PROFILE_TEMPLATE = {
+   PlayerName = "",
+   LastUpdated = 0,
    Cash = 0,
    Items = {},
 }
